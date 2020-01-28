132eb739ccdcef4b7b2baa9cca1063fa4bf07f28
Trin3232 <emanuel6464@mail.com>
2020-01-28 17:14:42 +0000

    update markup & css implemented js code for animation

---
+++ main.js
@@ -0,0 +1,21 @@
+setInterval(setMyClock, 1000);
+
+const secondHand = document.querySelector("[data-second-hd]");
+const minuteHand = document.querySelector("[data-minute-hd]");
+const hourHand = document.querySelector("[data-hour-hd]");
+
+function setMyClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setMyClock();
